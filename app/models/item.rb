87bc39b7d6158b1135fb83b88a053cf006b35407
Mattan Griffel <mattan@onemonth.com>
2016-10-25 14:57:34 -0400

    Add validations to Tweets

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
